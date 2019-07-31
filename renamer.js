2f429f6a0c8401241ded4734d7462068bdbe9a68
James Hennigan <James.Hennigan@CW.local>
2019-07-31 13:24:53 +0100

    fixed NaN appearing at start of renamed file

--- renamer.js
+++ renamer.js
@@ -4,15 +4,18 @@ function main(args){
 	console.log(args);
 	const sFolderName = args[2];
 	const sStringToFind = args[3];
-	const sStringToReplaceWith = args[4];
+	let sStringToReplaceWith = args[4];
+	if(typeof sStringToReplaceWith === "undefined"){
+		sStringToReplaceWith = "";
+	}
 	if(typeof sFolderName === "undefined"){
-		throw new Error("Directory doesnt exist");
+			throw new Error("Directory doesnt exist");
 	}else{
 		const dirs = fs.readdirSync(sFolderName);
 		dirs.forEach(function(file){
 			if(file.indexOf(sStringToFind) > -1){
 				console.log(file+" will be renamed");
-				fs.renameSync(sFolderName+file,+sFolderName+file.replace(sStringToFind,sStringToReplaceWith));
+				fs.renameSync(sFolderName+file,sFolderName+file.replace(sStringToFind,sStringToReplaceWith));
 			}
 		});
 	}
